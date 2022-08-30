8a483a259132fd61e30d74180e58f8b629af3321
Rajeev SD <rajeevratnasd@gmail.com>
2022-08-30 21:16:50 +1000

    First Commit

---
+++ src/main/java/com/demo/employeemanagerh2/EmployeemanagerH2Application.java
@@ -0,0 +1,13 @@
+package com.demo.employeemanagerh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagerH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagerH2Application.class, args);
+	}
+
+}
